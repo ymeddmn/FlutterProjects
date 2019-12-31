afd406aca0223fe4363ce5262e493a1af75bbd22
mage <1275054668@qq.com>
2019-12-31 17:11:42 +0800

    使用Container显示图片

--- Container/flutter_app/lib/main.dart
+++ Container/flutter_app/lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -25,19 +24,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Container(
-        padding: EdgeInsets.all(10),
         alignment: Alignment.center,
-        child: Image.network("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2032467668,2180843216&fm=26&gp=0.jpg"),
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.yellow,
+        child: Container(
+          width: 300,
+          height: 300,
+          padding: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          //设置Container内部child的位置
+          decoration: BoxDecoration(
+              border: Border.all(width: 5, color: Colors.red),
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              image: DecorationImage(image: NetworkImage("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2032467668,2180843216&fm=26&gp=0.jpg"))
+          ),
 
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }

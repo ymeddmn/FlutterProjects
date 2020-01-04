e628e9d61b0897252b317f82084f2ee9c8218b1f
mage <1275054668@qq.com>
2020-01-04 12:35:32 +0800

    FlatButton

---
+++ flatbutton/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FlatButton(
+            onPressed: (){
+              print("打印哈哈哈");
+            },
+            child: Container(
+
+              alignment: Alignment.center,
+              width: 100,
+              height: 60,
+              color: Colors.green,
+              child: Text("哈哈哈",style: TextStyle(color: Colors.white),),
+            )),
+      ),
+    );
+  }
+}

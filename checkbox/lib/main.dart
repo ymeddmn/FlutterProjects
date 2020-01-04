6f556815bab1d222fbab7cd70b1c1a5197998f94
mage <1275054668@qq.com>
2020-01-04 21:43:09 +0800

    CheckBox,ios的部分被删除了

---
+++ checkbox/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool check = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Checkbox(
+              value: check,
+              onChanged: (bool change) {
+                setState(() {
+                  check = !check;
+                });
+              },
+              activeColor: Colors.green,
+              checkColor: Colors.pink,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

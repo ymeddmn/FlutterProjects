768004c634474da499e55e2eb11cae216730ebc1
mage <1275054668@qq.com>
2020-01-09 14:58:22 +0800

    AspectRatio

---
+++ aspectratio/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //AspectRatio 一个widget，试图将子widget的大小指定为某个特定的长宽比
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              color: Colors.green,
+              child: AspectRatio(//改变的是父widget的长宽
+                aspectRatio: 3,//长宽比
+                child: Container(
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

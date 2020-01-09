f1eb852e54f0bebb472728cd5cd3a6c7a7ddea54
mage <1275054668@qq.com>
2020-01-09 21:37:38 +0800

    OverflowBox

---
+++ overflowbox/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.red,
+          padding: EdgeInsets.fromLTRB(30, 30, 0, 0),
+          child: OverflowBox(
+            alignment: Alignment.topLeft,
+            maxWidth: 200,//必须设置
+            maxHeight: 200,//必须设置
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

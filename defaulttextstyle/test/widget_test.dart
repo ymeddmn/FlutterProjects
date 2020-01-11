3aedd4d0bf17d7455d90927f28d5b07a1bb54d63
mage <1275054668@qq.com>
2020-01-11 21:20:21 +0800

    DefaultTextStyle

--- defaulttextstyle/test/widget_test.dart
+++ defaulttextstyle/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:listbody/main.dart';
+import 'package:defaulttextstyle/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

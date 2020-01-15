2c1ee2c5e5edbddac0b525850d9861e2e9784fce
mage <1275054668@qq.com>
2020-01-15 10:24:43 +0800

    RatationTransition

---
+++ rotationtransition/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 5000),
+        lowerBound: 0,
+        upperBound: 2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RotationTransition(
+              turns: _controller,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.green,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _controller.forward();
+              },
+              child: Text('点击开始动画'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

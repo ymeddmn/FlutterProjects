b28abeb874a535dcd2707578c06b7b6db67b82ba
mage <1275054668@qq.com>
2020-01-16 17:38:00 +0800

    SizeTransition

---
+++ sizetransition/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this,duration: Duration(milliseconds: 2000));
+    _controller
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+      });
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);//这里的值必须是0-1之间
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SizeTransition(
+          axis: Axis.vertical,//horizonal:表示水平方向缩放widget，vertical：表示竖直方向缩放widget
+          sizeFactor: _animation,
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.green,
+          ),
+        ),
+      ),
+    );
+  }
+}
